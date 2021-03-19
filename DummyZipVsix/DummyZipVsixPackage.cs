a7d5fc8207d9ffdbca4281a5af532f7ae695e932
Tony Hallett <tonyhallett74@gmail.com>
2021-03-19 16:07:10 +0000

    conflict

--- DummyZipVsix/DummyZipVsixPackage.cs
+++ DummyZipVsix/DummyZipVsixPackage.cs
@@ -26,7 +26,7 @@ namespace DummyZipVsix
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [Guid(DummyZipVsixPackage.PackageGuidString)]
     public sealed class DummyZipVsixPackage : AsyncPackage
-    {//
+    {//yyy
         /// <summary>
         /// DummyZipVsixPackage GUID string.
         /// </summary>
