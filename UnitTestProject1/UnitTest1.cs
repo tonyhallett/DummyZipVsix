105c6f5618a535bbdbdc20e55de18adf0082b285
Tony Hallett <tonyhallett74@gmail.com>
2021-04-06 15:04:16 +0100

    fail test

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -9,6 +9,7 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestMethod1()
         {
+            throw new Exception("Should stop further job and not conclusion success")
         }
     }
 }
